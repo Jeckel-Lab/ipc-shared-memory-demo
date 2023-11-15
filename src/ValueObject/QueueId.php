961fd9500c11c5279a1d92a045ddf3c78c9b0f80
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2023-11-15 11:59:44 +0100

    Cleanup code

---
+++ src/ValueObject/QueueId.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 15/11/2023
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\IpcSharedMemoryDemo\ValueObject;
+
+class QueueId
+{
+
+}
